fcce14dcd09087300e5a6b116d171ed232f4beff
Rbeninche <rbeninche@gmail.com>
2021-07-28 17:43:44 -0400

    Added validation

--- CodingEvents/Models/Event.cs
+++ CodingEvents/Models/Event.cs
@@ -8,23 +8,34 @@ namespace CodingEvents.Models
     public class Event
     {
         public string Name { get; set; }
+
         public string Description { get; set; }
 
         public string ContactEmail { get; set; }
+
+        public string Location { get; set; }
+
+        public bool RegistrationRequired { get; set; }
+
+        public int NumberOfAttendees { get; set; }
+
         public int Id { get; }
-        private static int nextId = 1;
+        static private int nextId = 1;
 
-        public Event()
-        {
-            Id = nextId;
-            nextId++;
-        }
-
-        public Event(string name, string description, string contactEmail) : this()
+        public Event(string name, string description, string contactEmail, string location, bool registration, int attendees)
         {
             Name = name;
             Description = description;
             ContactEmail = contactEmail;
+            Location = location;
+            RegistrationRequired = registration;
+            NumberOfAttendees = attendees;
+            Id = nextId;
+            nextId++;
+        }
+
+        public Event()
+        {
             Id = nextId;
             nextId++;
         }
--- CodingEvents/ViewModel/AddEventViewModel.cs
+++ CodingEvents/ViewModel/AddEventViewModel.cs
@@ -8,15 +8,27 @@ namespace CodingEvents.ViewModel
 {
     public class AddEventViewModel
     {
-        [Required(ErrorMessage ="Name is required")]
-        [StringLength(50, MinimumLength =3, ErrorMessage ="Name must be between 3 and 50 characters")]
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters.")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Please enter a description for your event")]
-        [StringLength(500, ErrorMessage = "Description is too long")]
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(500, ErrorMessage = "Description too long!")]
         public string Description { get; set; }
 
         [EmailAddress]
         public string ContactEmail { get; set; }
+
+        [Required(ErrorMessage = "Location information is required.")]
+        public string Location { get; set; }
+
+        public bool IsTrue { get { return true; } }
+
+        [Compare("IsTrue", ErrorMessage = "Registration is required.")]
+        public bool RegistrationRequired { get; set; }
+
+        [Range(0, 100000, ErrorMessage = "The number of attendees must be between 0 and 100,000.")]
+        public int NumberOfAttendees { get; set; }
+
     }
 }
