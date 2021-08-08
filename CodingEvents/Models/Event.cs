50327b608cf48e73ab1c6c73250914e03425d569
Rbeninche <rbeninche@gmail.com>
2021-08-08 17:12:10 -0400

    Added eventcategory

--- CodingEvents/Models/Event.cs
+++ CodingEvents/Models/Event.cs
@@ -19,6 +19,8 @@ namespace CodingEvents.Models
 
         public int NumberOfAttendees { get; set; }
 
+        public EventType Type { get; set; }
+
         public int Id { get; }
         static private int nextId = 1;
 
--- CodingEvents/ViewModel/AddEventViewModel.cs
+++ CodingEvents/ViewModel/AddEventViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -29,6 +31,16 @@ namespace CodingEvents.ViewModel
 
         [Range(0, 100000, ErrorMessage = "The number of attendees must be between 0 and 100,000.")]
         public int NumberOfAttendees { get; set; }
+
+        public EventType Type { get; set; }
+
+        public List<SelectListItem> EventTypes { get; set; } = new List<SelectListItem>
+        {
+            new SelectListItem(EventType.Conference.ToString(), ((int)EventType.Conference).ToString()),
+            new SelectListItem(EventType.Meetup.ToString(), ((int)EventType.Meetup).ToString()),
+            new SelectListItem(EventType.Social.ToString(), ((int)EventType.Social).ToString()),
+            new SelectListItem(EventType.Workshop.ToString(), ((int)EventType.Workshop).ToString())
+        };
 
     }
 }
